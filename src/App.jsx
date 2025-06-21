34bd64b90ad32a94c43ef06741c3e15b59a5aba4
Romesh Tripathi <romeshktripathi@gmail.com>
2025-06-21 11:16:39 +0530

    UI Modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { day_blizzard } from "./assets/index.js";
 
 function App() {
+    const obj = {
+        time: "day",
+        weather: "clean",
+    };
     return (
-        <div className=" bg-[url('../public/clouds.jpg')] bg-cover h-screen text-neutral-200 overflow-auto">
+        <div
+            className={` bg-[url('../public/night_clean.jpg')] bg-cover h-screen text-neutral-200 overflow-auto`}
+        >
             <div className="p-4 lg:w-3/5 lg:mx-auto">
                 <LocationDate />
                 <div className="flex flex-col max-md:gap-8 md:flex-row gap-4 justify-between items-center  ">
@@ -12,7 +18,9 @@ function App() {
                     </Wrapper>
                 </div>
                 <div className="max-md:hidden">
-                    <h1 className="font-thin text-2xl my-4">Today's Weather</h1>
+                    <h1 className="font-medium text-2xl my-4">
+                        Today's Weather
+                    </h1>
                     <div className="flex gap-4 justify-evenly">
                         <TodayForcast />
                         <TodayForcast />
@@ -23,8 +31,10 @@ function App() {
                         <TodayForcast />
                     </div>
                 </div>
-                <h1 className="font-thin text-2xl my-4">Next 5 Days</h1>
+                <h1 className="font-medium text-2xl my-4">Next 5 Days</h1>
                 <div className="flex md:flex-col gap-4 overflow-auto ">
+                    <ForcastCard />
+                    <ForcastCard />
                     <ForcastCard />
                     <ForcastCard />
                     <ForcastCard />
@@ -61,7 +71,7 @@ function ForcastCard({
 }) {
     return (
         <>
-            <div className="flex shrink-0 max-md:flex-col justify-evenly md:w-full text-white items-center p-2 max-md:rounded-md max-md:bg-amber-300 ">
+            <div className="flex shrink-0 max-md:flex-col justify-evenly md:w-full text-white items-center p-2 max-md:rounded-md max-md:bg-black/30 ">
                 <div>
                     <div>{time}</div>
                     <div className="max-md:hidden">{date}</div>
@@ -144,8 +154,8 @@ function CurrentTemprature({ temprature, type }) {
                 {<img className="w-2/3 " src={day_blizzard} />}
             </div>
             <div className="w-1/2 flex flex-col justify-center items-center">
-                <div className="text-5xl font-thin ">21&deg; C</div>
-                <span className="text-lg">Mostly Sunny</span>
+                <div className="text-5xl font-thin ">21&deg;C</div>
+                <span className="text-lg font-light">Mostly Sunny</span>
             </div>
         </div>
     );
@@ -154,7 +164,7 @@ function LocationDate({ location = "London, UK", date = "Monday 29 August" }) {
     return (
         <div className="my-2 flex flex-col gap-2">
             <div className="text-3xl font-medium font-sans">{location}</div>
-            <div className="font-thin text-lg">{date}</div>
+            <div className="font-light text-lg">{date}</div>
         </div>
     );
 }
