46eaf0e4c270fdc3ab6fbd3b76b29c52e12a66d6
Romesh Tripathi <romeshktripathi@gmail.com>
2025-06-22 09:41:41 +0530

    Geolocation

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,9 +1,34 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-const authorization = {
-    key: "3ff9e951a96c46d0ba632912252106",
-};
+
 const url = "http://api.weatherapi.com/v1";
+// const api = axios.create({
+//     baseURL: "http://api.weatherapi.com/v1",
+//     params: {
+//         key: "3ff9e951a96c46d0ba632912252106",
+//     },
+// });
+// const authorization = {
+//     key: "3ff9e951a96c46d0ba632912252106",
+// };
+
+// const useFetchData = (endpoint) => {
+//     const [data, setData] = useState(null);
+//     const [loading, setLoading] = useState(true);
+//     const [error, setError] = useState(null);
+
+//     useEffect(() => {
+//         api.get(endpoint)
+//             .then((res) => setData(res.data))
+//             .catch((err) => setError(err))
+//             .finally(() => setLoading(false));
+//     }, [endpoint]);
+
+//     return { data, loading, error };
+// };
+
+// export default useFetchData;
+
 export function getCurrentWeather() {
     const [currentWeather, setCurrentWeather] = useState({});
     const [loading, setLoading] = useState(true);
@@ -12,7 +37,7 @@ export function getCurrentWeather() {
             .get(url + "/forecast.json", {
                 params: {
                     days: 5,
-                    q: "Mathura",
+                    q: userLocation(),
                     key: "3ff9e951a96c46d0ba632912252106",
                 },
             })
@@ -60,6 +85,7 @@ function userLocation() {
             (position) => {
                 const { latitude, longitude } = position.coords;
                 location = { latitude, longitude };
+                console.log(location);
             },
             (error) => {
                 console.error("Error getting user location:", error);
